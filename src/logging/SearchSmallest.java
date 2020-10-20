3308c072341a18a0e20ee448433abbb936d224a3
Walentin <valession@gmail.com>
2020-10-20 10:56:42 +0300

    Прологичена программа

--- src/logging/SearchSmallest.java
+++ src/logging/SearchSmallest.java
@@ -1,14 +1,15 @@
 package logging;
 
 import java.io.FileInputStream;
+import java.util.logging.Level;
 import java.util.logging.LogManager;
 import java.util.logging.Logger;
 
 
 public class SearchSmallest {
-    static Logger logger;
+    private static Logger logger;
     static {
-        try(FileInputStream configFile = new FileInputStream("C:\\Users\\Walentin\\LoggerConfigs\\configuration.config")){
+        try(FileInputStream configFile = new FileInputStream("C:\\Users\\Walentin\\IdeaProjects\\Software Design Instruments\\src\\logging\\loggerConfigurations\\logger.config")){
             LogManager.getLogManager().readConfiguration(configFile);
             logger = Logger.getLogger(SearchSmallest.class.getName());
         }catch (Exception ignore){
@@ -16,22 +17,29 @@ public class SearchSmallest {
         }
     }
     private static int function(int x) {
+        logger.log(Level.INFO, "Выполнение функции задачи");
         return (x * x - 9) * (x * x - 9) + 5;
     }
 
     public static void main (String[] args) {
+        logger.log(Level.INFO, "Начало программы, инициализация переменных");
         int a, b, t, M, R;
+        logger.log(Level.INFO, "Присваивание значении для переменных");
         a = -13;
         b = 13;
         M = a;
         R = function(a);
+        logger.log(Level.INFO, "Вход в цикл программы");
         for (t = a; t <= b; t++) {
+            logger.log(Level.INFO, "Проверка условия { function(t) < R} ");
             if (function(t) < R) {
+                logger.log(Level.INFO, "Условие { function(t) < R} верно ");
                 M = t;
                 R = function(t);
             }
         }
         System.out.println(M + 7);
+        logger.log(Level.INFO, "Конец программы");
 
     }
 }
